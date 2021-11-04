c0acd891aea64301690b2315b96cc1e5f1470df7
csolis1 <celida.solis@outlook.com>
2021-11-03 20:37:26 -0400

    adj, nouns & verb prompt

---
+++ script.js
@@ -0,0 +1,4 @@
+let adjective = prompt("Enter an adjective", "Example: chunky, fuzzy, sexy")
+let noun = prompt ("Enter a noun", "Example: person, place or thing");
+let noun2 = prompt("Enter another noun", "Example: person, place or thing");
+let verb = prompt("Enter a verb", "Example: run, jump, fly");
