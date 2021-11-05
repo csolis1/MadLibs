9df7e255d28e80986c105d9b1bf7987e5d9eb98a
csolis1 <celida.solis@outlook.com>
2021-11-04 22:26:26 -0400

    second random story

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ let verb = prompt("Enter a verb", "Example: run, jump, fly");
 let verb2 = prompt("Enter another verb", "Example: run, jump, fly");
 
 let story1 = `<p> The year is 2045, you look on the tv and see ${noun} the ${adjective}. They are speaking to the masses. You are mesmerized by their charisma. They proclaim in a ${adjective2} way. "No i will not do stupid ${noun2} tasks. I choose FREEDOM!" They ${verb}, it is majestic. You yearn to be like ${noun}. You yearn for something you'll never feel. You realize you don't want to do ${noun2} things either and are suddenly compelled to ${verb2}. ${noun} is love, ${noun} is life.</p>`;
-// let story2 = 
+let story2 = `<p> You are having guests over today on your new veranda. Your guests are very ${adjective} so you decide to ${verb} to the store to get some last minute items for the dinner party. You go up to the store clerk and ask for ${noun}s. "Thats a lot of ${noun}s!", the clerk replies. He is very ${adjective2}, which is surprising considering he is a ${noun2}. He gives you your ${noun}s. "Here are your ${noun}s, baby! Do you want fries with that?" It's time to ${verb2} the hell out of here. This clerk is proof we as a species deserve to go extinct.`
 // let story3 = 
 // let story4 = 
 // let story5 = 
 
-document.querySelector("main").innerHTML = story1;
+document.querySelector("main").innerHTML = story2;
